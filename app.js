9a541858273b3d5ae471125ba691169f6bd4788c
Adeniyi <161259943+BEV-V@users.noreply.github.com>
2024-07-22 09:15:17 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+function handleClick(element) {
+    element.style=`background-color:green
+                    background-color: red;
+                    width: 200px;
+                    height: 50px;
+                    border-radius: 10px 20px;
+                    box-shadow: black;
+                    box-shadow: black 20px 20px 20px;
+                    margin-bottom: 50px;`
+    
+}
